f29603172d0f0dacde714ebebf5580d08f168744
Alexander Titov <titoffag@gmail.com>
2020-11-21 20:18:39 +0400

    adding global styles using styled-components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,30 @@
-import '../styles/globals.css';
+import { AppProps } from 'next/app';
+import { createGlobalStyle } from 'styled-components';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+const GlobalStyle = createGlobalStyle`
+  html,
+  body {
+    padding: 0;
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+`;
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  );
 }
-
-export default MyApp;
