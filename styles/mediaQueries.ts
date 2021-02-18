bf96a5af8300bccf35438a0d44f47309daffe8f9
ShevtsovAnton <a0660576041@gmail.com>
2021-02-18 12:18:32 +0200

    add global variable for css media query

---
+++ styles/mediaQueries.ts
@@ -0,0 +1,11 @@
+export const breakpoints = {
+	tablet: 992
+};
+
+export const size = {
+	tablet: `${breakpoints.tablet}px`
+};
+
+export const device = {
+	tablet: `(max-width: ${size.tablet})`
+};
