b7768193a95b6ecf1a8036b6e106a1f629316a2f
ShevtsovAnton <a0660576041@gmail.com>
2021-03-22 16:36:38 +0200

    feat: add DigestList, DigestItem, DigestSubscribe components

---
+++ components/Digests/index.js
@@ -0,0 +1,9 @@
+import DigestItem from './DigestItem';
+import DigestsList from './DigestsList';
+import DigestSubscribe from './DigestSubscribe';
+
+export {
+  DigestItem,
+  DigestsList,
+  DigestSubscribe
+};
