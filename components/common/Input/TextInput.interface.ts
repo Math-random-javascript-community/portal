3a8339661aabe5d44b858e2515255b56487d8d96
Anton <a0660576041@gmail.com>
2021-04-22 17:16:20 +0300

    temp fix textInput typescript issue

--- components/common/Input/TextInput.interface.ts
+++ components/common/Input/TextInput.interface.ts
@@ -1,4 +1,6 @@
 import { InputHTMLAttributes, TextareaHTMLAttributes } from 'react';
+
+//TODO make TextInput and TextArea separate components
 type CommonProps = {
   value: string;
   name: string;
@@ -9,15 +11,11 @@ type CommonProps = {
   hasValidValue?: boolean;
   requiredMessage?: string;
   rows?: number;
-  inputType: 'input' | 'textarea';
-}
+  inputType?: 'input' | 'textarea';
+};
 
-export interface StyledTextInputProps extends InputHTMLAttributes<HTMLInputElement> { }
+export interface StyledTextInputProps extends InputHTMLAttributes<HTMLInputElement> {}
 export interface StyledTextAreaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {}
 
-export type TextAreaProps =  StyledTextAreaProps & CommonProps;
-export type TextInputProps =  StyledTextInputProps & CommonProps;
-
-
-
-
+export type TextAreaProps = StyledTextAreaProps & CommonProps;
+export type TextInputProps = StyledTextInputProps & CommonProps;
